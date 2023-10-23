e0f09023df5451193d497aaa3c490db0995ee407
Ulyana Atanova <101881077+UlyanaAt@users.noreply.github.com>
2023-10-23 18:22:24 +0700

    Add files via upload

---
+++ first_lab.sh
@@ -0,0 +1,58 @@
+#! /usr/bin/bash -e
+
+function delTempDir {
+#moving to a upper directory
+cd ../ 
+
+#deleting temporary directory
+if [ -d "$directoryName" ]; then
+    echo "Do you want to delete temporary directory? (Y/N)"
+    read answer
+    if [ "$answer" = "Y" ]; then
+        rm -d $directoryName
+        echo "Temporary directory was deleted"
+    fi
+fi
+}
+
+#Trapping signals
+trap "delTempDir; exit 1" SIGQUIT SIGTERM SIGINT
+
+if [ "$#" -lt 1 ]; then
+    echo "You didn't enter file name. Example: helloW.cpp"
+    exit 1
+fi
+
+sourceFile=$1
+
+#creation of the temporary directory 
+directoryName=`mktemp -d authentic+XXXXXX`
+
+#moving the source file to a temporary directory
+mv $1 $directoryName
+
+#moving to a temporary directory
+cd $directoryName
+
+#finding the output file name in the source file
+fileName=`awk '/\/\/Output:/{print $2}' $1`
+
+if [ -z $fileName ]; then
+    echo "Your file doesn't contain the output file name"
+    mv $1 ../
+    cd ../
+    rmdir $directoryName
+    exit 1
+fi
+
+#source file compilation
+g++ -Wall -o $fileName $1
+
+#output file execution
+./$fileName
+
+#moving a source and output files to a upper directory
+mv $sourceFile ../
+mv $fileName ../
+
+delTempDir
